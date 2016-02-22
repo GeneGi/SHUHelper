27eb065fae6e89c881dfdb1b1b5421182d4423d2
GeneGi <junzheji@gmail.com>
2016-02-22 15:03:53 +0800

    add a simple UI for SiteBlocker

--- src/SiteBlocker.java
+++ src/SiteBlocker.java
@@ -1,50 +1,56 @@
+import javax.swing.*;
+import java.awt.*;
 import java.io.*;
 import java.nio.channels.FileChannel;
-import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class SiteBlocker {
-    Scanner in = new Scanner(System.in);
+public class SiteBlocker extends JFrame{
     File hosts;
+    JPanel north;
+    JTextArea textArea;
+    JButton blockSite;
+    JButton unblockSite;
 
-    public static void main(String[] args) {
-        SiteBlocker siteBlocker = new SiteBlocker();
-        siteBlocker.run();
+    public SiteBlocker() {
+        initUI();
+        hosts = getHosts();
+        showBlockList();
+    }
+    private void initUI() {
+        north = new JPanel(new GridLayout(1, 2));
+        textArea = new JTextArea();
+        blockSite = new JButton("屏蔽网站");
+        unblockSite = new JButton("取消屏蔽");
+
+        textArea.setEditable(false);
+
+        north.add(blockSite);
+        north.add(unblockSite);
+        add(north, BorderLayout.NORTH);
+        add(textArea, BorderLayout.CENTER);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("网站屏蔽");
+        setVisible(true);
+        setSize(400, 300);
+
+        blockSite.addActionListener(e -> {
+            String site = JOptionPane.showInputDialog("请输入你要屏蔽的网站地址:");
+            block(site);
+            showBlockList();
+        });
+
+        unblockSite.addActionListener(e -> {
+            String site = JOptionPane.showInputDialog("请输入你要取消屏蔽的网站地址:");
+            unblock(site);
+            showBlockList();
+        });
+
     }
 
-    public void run() {
-        getHosts();
-        int order = 1;
-        while (order != 0) {
-            menu();
-            order = in.nextInt();
-            switch (order) {
-                case 1: {
-                    block();
-                    break;
-                }
-                case 2: {
-                    showList();
-                    break;
-                }
-                case 3: {
-                    unblock();
-                    break;
-                }
-            }
-        }
-    }
-
-    private void menu() {
-        System.out.println("1-屏蔽网站");
-        System.out.println("2-已屏蔽列表");
-        System.out.println("3-取消屏蔽");
-        System.out.println("0-退出");
-        System.out.print("请输入命令:");
-    }
-
-    private void getHosts() {
+    private File getHosts() {
         String fileName = "";
         String OS = System.getProperty("os.name").toLowerCase();
         if ((OS.indexOf("nix")) >= 0) {
@@ -56,22 +62,42 @@ public class SiteBlocker {
         } else {
             System.out.println("未识别的操作系统!");
         }
-        hosts = new File(fileName);
+        return new File(fileName);
     }
 
-    private void block() {
-        System.out.println("请输入你要屏蔽的网站:");
-        String site = in.next();
+    private void block(String site) {
         try {
             Writer output;
             output = new BufferedWriter(new FileWriter(hosts, true));
-            output.append("127.0.0.1    " + site);
+            output.append("127.0.0.1    " + site + "\n");
             output.close();
         } catch (IOException e) {
-            System.out.println("没有足够的权限,请使用sudo java SiteBlocker 运行");
+            System.out.println("没有足够的权限,请使用管理员权限运行");
             e.printStackTrace();
         }
-        System.out.println("屏蔽成功!");
+    }
+
+    private void unblock(String site) {
+        try {
+            File tempFile = new File("tempHosts");
+            BufferedReader reader = new BufferedReader(new FileReader(hosts));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+            String lineToRemove = "127.0.0.1    " + site;
+            String currentLine;
+
+            while ((currentLine = reader.readLine()) != null) {
+                String trimmedLine = currentLine.trim();
+                if (trimmedLine.equals(lineToRemove)) continue;
+                writer.write(currentLine + System.getProperty("line.separator"));
+            }
+            writer.close();
+            reader.close();
+            tempFile.renameTo(hosts);
+
+        } catch (IOException e) {
+
+        }
     }
 
     public void backup(File file) throws IOException {
@@ -111,48 +137,33 @@ public class SiteBlocker {
         return false;
 
     }
-    private void showList() {
+    private ArrayList getBlockList() {
+        ArrayList<String> list = new ArrayList<>();
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(hosts));
             String line = bufferedReader.readLine();
             while (line != null) {
                 if (isBlock(line)) {
-                    System.out.println(line.substring(12));
+                    list.add(line.substring(14) + "\n");
                 }
                 line = bufferedReader.readLine();
             }
         } catch (IOException e) {
         }
+        return list;
     }
-    private void unblock() {
-        System.out.println("当前屏蔽网站列表:");
-        showList();
-        System.out.println("请输入你想要取消屏蔽的网站地址:");
-        String unblockSite = in.next();
-        System.out.println(unblockSite);
-        try {
-            File tempFile = new File("tempHosts");
-            BufferedReader reader = new BufferedReader(new FileReader(hosts));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-
-            String lineToRemove = "127.0.0.1    " + unblockSite;
-            String currentLine;
-
-            while ((currentLine = reader.readLine()) != null) {
-                String trimmedLine = currentLine.trim();
-                if (trimmedLine.equals(lineToRemove)) continue;
-                writer.write(currentLine + System.getProperty("line.separator"));
-            }
-            writer.close();
-            reader.close();
-            boolean successful = tempFile.renameTo(hosts);
-            if (successful) {
-                System.out.println("取消成功!");
-            } else {
-                System.out.println("出现了错误!");
-            }
-        } catch (IOException e) {
-
+    private void showBlockList() {
+        textArea.setText("已屏蔽列表:\n");
+        ArrayList<String> list = getBlockList();
+        for (String line : list) {
+            textArea.append(line);
         }
+    }
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            SiteBlocker siteBlocker = new SiteBlocker();
+        });
     }
 }
